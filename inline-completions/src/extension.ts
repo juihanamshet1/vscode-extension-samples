e49d044fb954c87d676bbcc63a1b73d83d9bedf0
Jui Hanamshet (SHE/HER) <juihanamshet@microsoft.com>
2022-10-20 16:57:32 -0400

    adding snippet support and updating package.json

--- inline-completions/src/extension.ts
+++ inline-completions/src/extension.ts
@@ -1,52 +1,49 @@
-import * as vscode from 'vscode';
-
-// Try it out in `playground.js`
-
-export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand(
-		'extension.inline-completion-settings',
-		() => {
-			vscode.window.showInformationMessage('Show settings');
-		}
-	);
-
-	context.subscriptions.push(disposable);
-	let someTrackingIdCounter = 0;
-
-	const provider: vscode.InlineCompletionItemProvider = {
-		provideInlineCompletionItems: async (document, position, context, token) => {
-			console.log('provideInlineCompletionItems triggered');
-
-			const regexp = /\/\/ \[(.+),(.+)\):(.*)/;
-			if (position.line <= 0) {
-				return;
-			}
-
-			const lineBefore = document.lineAt(position.line - 1).text;
-			const matches = lineBefore.match(regexp);
-			if (matches) {
-				const start = matches[1];
-				const startInt = parseInt(start, 10);
-				const end = matches[2];
-				const endInt =
-					end === '*' ? document.lineAt(position.line).text.length : parseInt(end, 10);
-				const insertText = matches[3].replace(/\\n/g, '\n');
-
-				return [
-					{
-						insertText,
-						range: new vscode.Range(position.line, startInt, position.line, endInt),
-						someTrackingId: someTrackingIdCounter++,
-					},
-				] as MyInlineCompletionItem[];
-			}
-		},
-	};
-
-	vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, provider);
-
-}
-
-interface MyInlineCompletionItem extends vscode.InlineCompletionItem {
-	someTrackingId: number;
+// @ts-check
+/// <reference path="C:\\dev\\microsoft\\vscode\\src\\vscode-dts\\vscode.proposed.inlineCompletionsAdditions.d.ts" />
+/// <reference path="C:\\dev\\microsoft\\vscode\\src\\vscode-dts\\vscode.d.ts" />
+const vscode = require('vscode');
+const { window, Range } = vscode;
+/**
+ * @type {(e: vscode.ExtensionContext) => void}
+*/
+exports.activate = function (context) {
+    console.log('hello world');
+    vscode.commands.registerCommand('demo-ext.command1', async (...args) => {
+        vscode.window.showInformationMessage('command1: ' + JSON.stringify(args));
+    });
+    /**
+     * @type vscode.InlineCompletionItemProvider
+    */
+    const provider = {
+        provideInlineCompletionItems: async (document, position, context, token) => {
+            console.log('provideInlineCompletionItems triggered');
+            const regexp = /\/\/ \[(.+?),(.+?)\)(.*?)\:(.*)/;
+            if (position.line <= 0) {
+                return;
+            }
+            const lineBefore = document.lineAt(position.line - 1).text;
+            const matches = lineBefore.match(regexp);
+            if (matches) {
+                const start = matches[1];
+                const startInt = parseInt(start, 10);
+                const end = matches[2];
+                const endInt = end === '*' ? document.lineAt(position.line).text.length : parseInt(end, 10);
+                const flags = matches[3];
+                const completeBracketPairs = flags.includes('b');
+                const isSnippet = flags.includes('s');
+                const text = matches[4].replace(/\\n/g, '\n');
+                return {
+                    items: [{
+                        insertText: isSnippet ? new vscode.SnippetString(text) : text,
+                        range: new Range(position.line, startInt, position.line, endInt),
+                        completeBracketPairs
+                    }],
+                    commands: [
+                        { command: 'demo-ext.command1', title: 'Hello World', arguments: [1, 2] }
+                    ]
+                };
+            }
+        }
+    };
+    vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, provider);
 }
